5cc65548522440c68dba56faa4060cf410a4b9b6
mykhailopavliv <mykhailopavliv95@gmail.com>
2018-09-25 02:39:33 +0300

    added custom rescue from :not_found and :bad_request errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::ParameterMissing, with: :bad_request
+  
+  private
+
+  def not_found
+    render json: { errors: 'Not found' }, status: :not_found
+  end
+
+  def bad_request
+    render json: { errors: 'Something went wrong' }, status: :bad_request
+  end
 end
